03fc322ae7280ad840709b1d848cde0c722109ce
Uwe L. Korn <uwe.korn@quantco.com>
2020-11-10 15:26:01 +0100

    Explicitly link to libz

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 set -x
 
 export LD_LIBRARY_PATH=$PREFIX/lib
+sed -ie 's/v8_enable_snapshot_compression = true/v8_enable_snapshot_compression = false/g' BUILD.gn
 
 if [[ $(uname) =~ .*Darwin.* ]]; then
   sed -ie "s;@PREFIX@;${PREFIX};g" build/config/mac/BUILD.gn
@@ -41,13 +42,19 @@ treat_warnings_as_errors=false
 fatal_linker_warnings=false
 enable_stripping=true
 checkout_google_benchmark=false
+v8_use_snapshot=false
 EOF
-  gn gen out.gn "--args=use_custom_libcxx=false clang_use_chrome_plugins=false v8_use_external_startup_data=false is_debug=false clang_base_path=\"${BUILD_PREFIX}\" is_component_build=true icu_use_system=true icu_include_dir=\"$PREFIX/include\" icu_lib_dir=\"$PREFIX/lib\" use_sysroot=false is_clang=false treat_warnings_as_errors=false fatal_linker_warnings=false enable_stripping=true"
+  gn gen out.gn "--args=use_custom_libcxx=false clang_use_chrome_plugins=false v8_use_external_startup_data=false is_debug=false clang_base_path=\"${BUILD_PREFIX}\" is_component_build=true icu_use_system=true icu_include_dir=\"$PREFIX/include\" icu_lib_dir=\"$PREFIX/lib\" use_sysroot=false is_clang=false treat_warnings_as_errors=false fatal_linker_warnings=false enable_stripping=true v8_use_snapshot=false"
   sed -i "s/ gcc/ ${HOST}-gcc/g" out.gn/toolchain.ninja
   sed -i "s/ g++/ ${HOST}-g++/g" out.gn/toolchain.ninja
   sed -i 's/deps = x86_64-conda-linux-gnu.*$//g' out.gn/toolchain.ninja
   sed -i 's/-Werror//g' out.gn/**/*.ninja
 fi
+
+# Explicitly link to libz, otherwise _compressBound cannot be found
+sed -ie "s/libs =/libs = -lz/g" out.gn/obj/v8.ninja
+sed -ie "s/libs =/libs = -lz/g" out.gn/obj/v8_for_testing.ninja
+
 ninja -C out.gn
 
 mkdir -p $PREFIX/lib
