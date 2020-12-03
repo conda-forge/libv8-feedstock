2f6e4115bd913e70add7c8812dc9e464369d375c
root <shivanij@us.ibm.com>
2020-12-03 06:03:14 -0500

    Enabling ppc64le support

--- recipe/build.sh
+++ recipe/build.sh
@@ -50,11 +50,17 @@ elif [[ "${target_platform}" =~ linux.* ]]; then
   echo 'fatal_linker_warnings=false'  >> build/config/gclient_args.gni
 
   gn gen out.gn "--args=use_custom_libcxx=false clang_use_chrome_plugins=false v8_use_external_startup_data=false is_debug=false clang_base_path=\"${BUILD_PREFIX}\" is_component_build=true icu_use_system=true icu_include_dir=\"$PREFIX/include\" icu_lib_dir=\"$PREFIX/lib\" use_sysroot=false is_clang=false treat_warnings_as_errors=false fatal_linker_warnings=false enable_stripping=true"
-  sed -i "s/ gcc/ ${HOST}-gcc/g" out.gn/toolchain.ninja
-  sed -i "s/ g++/ ${HOST}-g++/g" out.gn/toolchain.ninja
-  sed -i 's/deps = x86_64-conda-linux-gnu.*$//g' out.gn/toolchain.ninja
+  if [[ "${target_platform}" == "linux-ppc64le" ]]; then
+    sed -i "s/ gcc/ powerpc64le-conda_cos7-linux-gnu-gcc/g" out.gn/toolchain.ninja
+    sed -i "s/ g++/ powerpc64le-conda_cos7-linux-gnu-g++/g" out.gn/toolchain.ninja
+    sed -i 's/deps = powerpc64le-conda_cos7-linux-gnu.*$//g' out.gn/toolchain.ninja
+  else
+    sed -i "s/ gcc/ ${HOST}-gcc/g" out.gn/toolchain.ninja
+    sed -i "s/ g++/ ${HOST}-g++/g" out.gn/toolchain.ninja
+    sed -i 's/deps = x86_64-conda-linux-gnu.*$//g' out.gn/toolchain.ninja
+  fi
   sed -i 's/-Werror//g' out.gn/**/*.ninja
-
+  
   # ld.gold segfaults on mksnapshot linkage with binutils 2.35
   # for f in "out.gn/obj/bytecode_builtins_list_generator.ninja out.gn/obj/bytecode_builtins_list_generator.ninja out.gn/obj/v8.ninja out.gn/obj/v8_libbase.ninja out.gn/obj/v8_for_testing.ninja out.gn/obj/mksnapshot.ninja out.gn/obj/v8_simple_parser_fuzzer.ninja out.gn/obj/v8_simple_wasm_async_fuzzer.ninja out.gn/obj/v8_simple_wasm_fuzzer.ninja out.gn/obj/third_party/zlib/zlib.ninja out.gn/obj/cppgc_standalone.ninja"; do
   for f in "$(ls out.gn/obj/*.ninja) out.gn/obj/third_party/zlib/zlib.ninja"; do
