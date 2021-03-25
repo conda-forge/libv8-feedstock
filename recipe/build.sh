640bf4fbd3009fd65700efae5ea50e66c7fdc079
Uwe L. Korn <uwe.korn@quantco.com>
2021-03-25 22:46:49 +0100

    Handle aarch64 compilers

--- recipe/build.sh
+++ recipe/build.sh
@@ -43,7 +43,7 @@ elif [[ "${target_platform}" == "osx-arm64" ]]; then
   # Explicitly link to libz, otherwise _compressBound cannot be found
   sed -ie "s/libs =/libs = -lz/g" out.gn/obj/v8.ninja
   sed -ie "s/libs =/libs = -lz/g" out.gn/obj/v8_for_testing.ninja
-elif [[ "${target_platform}" =~ linux.* ]]; then
+elif [[ "${target_platform}" == linux-* ]]; then
   echo 'use_sysroot=false' >> build/config/gclient_args.gni
   echo 'is_clang=false' >> build/config/gclient_args.gni
   echo 'treat_warnings_as_errors=false' >> build/config/gclient_args.gni
@@ -52,6 +52,10 @@ elif [[ "${target_platform}" =~ linux.* ]]; then
   gn gen out.gn "--args=use_custom_libcxx=false clang_use_chrome_plugins=false v8_use_external_startup_data=false is_debug=false clang_base_path=\"${BUILD_PREFIX}\" is_component_build=true icu_use_system=true icu_include_dir=\"$PREFIX/include\" icu_lib_dir=\"$PREFIX/lib\" use_sysroot=false is_clang=false treat_warnings_as_errors=false fatal_linker_warnings=false enable_stripping=true"
   sed -i "s/ gcc/ $(basename ${CC})/g" out.gn/toolchain.ninja
   sed -i "s/ g++/ $(basename ${CXX})/g" out.gn/toolchain.ninja
+  sed -i "s/ aarch64-linux-gnu-gcc/ $(basename ${CC})/g" out.gn/toolchain.ninja
+  sed -i "s/ aarch64-linux-gnu-g++/ $(basename ${CXX})/g" out.gn/toolchain.ninja
+  sed -i "s/ ${HOST}-gcc/ $(basename ${CC})/g" out.gn/toolchain.ninja
+  sed -i "s/ ${HOST}-g++/ $(basename ${CXX})/g" out.gn/toolchain.ninja
   sed -i "s/deps = $(basename ${CC})\$//g" out.gn/toolchain.ninja
   sed -i "s/deps = $(basename ${CXX})\$//g" out.gn/toolchain.ninja
   sed -i 's/-Werror//g' out.gn/**/*.ninja
