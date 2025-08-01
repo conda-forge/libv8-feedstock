640247367da61514f3cabb6a832715b8878dd548
Daniel Nachun <daniel.nachun@gmail.com>
2025-07-31 21:39:37 -0500

    more fixes

--- recipe/build.sh
+++ recipe/build.sh
@@ -51,6 +51,7 @@ if [[ "${target_platform}" == "osx-64" ]]; then
   sed -i "s?-Wl,--no-warn-duplicate-rpath?-L${PREFIX}/lib -Wl,--no-warn-duplicate-rpath?" out.gn/obj/third_party/zlib/zlib.ninja
   sed -i "s?ldflags = -Wl,--no-warn-duplicate-rpath?ldflags = -L${PREFIX}/lib -Wl,--no-warn-duplicate-rpath?" out.gn/obj/v8_libbase.ninja
   sed -i "s?ldflags = -Wl,--no-warn-duplicate-rpath?ldflags = -L${PREFIX}/lib -Wl,--no-warn-duplicate-rpath?" out.gn/obj/v8_libplatform.ninja
+  sed -i "s?ldflags = -Wl,--no-warn-duplicate-rpath?ldflags = -L${PREFIX}/lib -Wl,--no-warn-duplicate-rpath?" out.gn/obj/bytecode_builtins_list_generator
 elif [[ "${target_platform}" == "osx-arm64" ]]; then
   echo 'mac_sdk_min="11.0"' >> build/config/gclient_args.gni
   gn gen out.gn "--args=target_cpu=\"arm64\" use_custom_libcxx=false clang_use_chrome_plugins=false v8_use_external_startup_data=false is_debug=false clang_base_path=\"${BUILD_PREFIX}\" mac_sdk_min=\"11.0\" is_component_build=true mac_sdk_path=\"${CONDA_BUILD_SYSROOT}\" icu_use_data_file=false icu_use_system=true icu_include_dir=\"$PREFIX/include\" icu_lib_dir=\"$PREFIX/lib\" enable_stripping=true clang_version=${clang_major_version}"
